f0d42dd356e794230bda3deeb58439112e0e22e9
Joel <joelluis73@gmail.com>
2025-02-18 19:06:29 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <>
-      <h3 className="text-red-500">Hello World</h3>
+      <h3>Hello World</h3>
       <Button>Click</Button>
     </>
   )
